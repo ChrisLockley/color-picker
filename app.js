08798212d69fc9b409ad1dde845192877d905ac7
Christian <christianlockley4@gmail.com>
2022-02-07 16:32:06 -0800

    completed the color picker js exercise

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const colors = ['teal', 'red', 'blue', 'orange', 'purple'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
 
+btn.addEventListener('click', () => {
+    const randomNumber = getRandomNumber();
+
+    document.body.style.background = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+}); 
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
